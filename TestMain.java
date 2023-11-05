d8b6ec5f5be2428149927371e68ba69066591a7d
naveendev9944 <naveendev994474@gmail.com>
2023-11-05 16:45:31 +0530

    Almost done

--- TestMain.java
+++ TestMain.java
@@ -5,20 +5,53 @@ public class TestMain{
     public static void main(String[] args) {
         SysAdminModule sys = new SysAdminModule();
         HRModule.registerListener(sys);
+	Scanner sc=new Scanner(System.in);
 
-        // Add an employee to the HR Module
-        byte g=1;
-        Employee employee = new Employee("John", 1, g,g);
-        HRModule.addEmployee(employee);
+	boolean loop = true;
+    	while (loop)
+      	   {
+		System.out.println("1.Add Employee  \n2.Show Employee\n3.Check Ip \n4.Exit");
+		int choice = sc.nextInt();
+		sc.nextLine();
+		switch (choice){
+	  		case 1:{
+	  			
+	     		        System.out.println ("Enter the Employee Name:");
+	     		        String name=sc.nextLine();
+	     		        System.out.println ("Enter the Employee id:");
+	     		        int id=sc.nextInt();
+	     		        System.out.println ("Enter the gender\n1.Male\n2.Female");
+	     		        byte gender=sc.nextByte();
+	     		       	System.out.println ("Enter the role\n1.Manger\n2.Others");
+	     		        byte role=sc.nextByte();
+				sc.nextLine();
+				Employee employee = new Employee(name, id, gender,role);
+        			HRModule.addEmployee(employee);
+				System.out.println(name+" was added as an employee\n");
+	     			break;
+	   		 }
+	 		 case 2:{
+				System.out.println("Employees in the organaization");
+				for (Employee emp : HRModule.getEmployees()) {
+            				Computer computer = emp.getComputer();
+            				System.out.println("Employee: " + emp.getName() + ", IP: " + computer.getIp());
+        			}
+	      			break;
+	    		}
 
-        // Check if SysAdminModule's isIPPresent method works
-        short ip=1000;
-        System.out.println("Is IP Present: " + sys.isIPPresent(ip));
+	  		case 3:{
+				System.out.println("Enter ip");
+	    			short ip=sc.nextShort();
+        			System.out.println("Is IP Present: " + sys.isIPPresent(ip));
+				break;
+	    		}
+	    		
+	  		case 4:{
+	      			loop=false;
+	 		 }
+      	   }
+       }
 
-        // Print all employees with their IP addresses
-        for (Employee emp : HRModule.getEmployees()) {
-            Computer computer = emp.getComputer();
-            System.out.println("Employee: " + emp.getName() + ", IP: " + computer.getIp());
-        }
+        
     }
 }
